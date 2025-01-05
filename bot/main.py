2c9ce82b3cbccd5f00105813bc108783d651cde2
Anton Muradov <anton.muradov@onlinesup.com>
2025-01-05 21:07:07 +0300

    add logger

--- bot/main.py
+++ bot/main.py
@@ -3,13 +3,17 @@ import logging
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from handlers import start
+from middleware.logger import LoggingMiddleware
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+dp.update.middleware(LoggingMiddleware())
+
 dp.include_router(start.router)
+
 
 async def main():
     await dp.start_polling(bot)
