ff5a47628f07a2560ef3b1e2a29f8ad91fd8fb9f
Anton Muradov <anton.muradov@onlinesup.com>
2025-01-06 20:15:49 +0300

    add pydantic

--- backend/app/api/routes/start.py
+++ backend/app/api/routes/start.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter()
 
 
-@router.get("/")
+class WelcomeResponse(BaseModel):
+    message: str
+
+
+@router.get("/", response_model=WelcomeResponse)
 async def start():
     message = "Hi there! It's your FastAPI app responding!"
     return {"message": message}
